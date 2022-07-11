e1e21f9743b3efc26073e7949961de32dd13e5a5
Tim <Not@myEmail.com>
2022-07-11 16:27:18 +0200

    changing some files + added MenuButtonTheme

--- WeatherApp/MainView.xaml.cs
+++ WeatherApp/MainView.xaml.cs
@@ -18,9 +18,9 @@ namespace WeatherApp
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainView : Window
     {
-        public MainWindow()
+        public MainView()
         {
             InitializeComponent();
         }
