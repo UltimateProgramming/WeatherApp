3ef82245efc6be7eb7a32494554d6b2eb12d9af7
Tim <Not@myEmail.com>
2022-07-24 18:19:23 +0200

    worked further on the Current Weather View

--- WeatherApp/ViewModels/CurrentWeatherViewVM.cs
+++ WeatherApp/ViewModels/CurrentWeatherViewVM.cs
@@ -5,10 +5,21 @@ namespace WeatherApp.ViewModels
     public class CurrentWeatherViewVM : BaseViewModel
     {
         private readonly string _title = "Current Weather";
-        private readonly string _locationLabel = "Location";
+        private readonly string _locationLbl = "Location:";
+        private readonly string _dateLbl = "Date:";
+        private readonly string _timeLbl = "Time:";
+        private string _temparature = "0";
 
         public string Title { get { return _title; } }
-        public string LocationLabel { get { return _locationLabel; } }
+        public string LocationLbl { get { return _locationLbl; } }
+        public string DateLbl { get { return _dateLbl; } }
+        public string TimeLbl { get { return _timeLbl; } }
+        public string Temparature
+        {
+            get { return _temparature + "°C"; }
+            set { SetProperty<string>(ref _temparature, value, nameof(Temparature)); }
+
+        }
 
     }
 }
